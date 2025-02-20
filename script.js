f12b1b1ec783f1643f96946441ee18910c244a85
achino <zakipbg4@gmail.com>
2025-02-20 06:09:12 +0000

    add sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+// const navbar = document.getElementById('sidebar')
+
+function toggleMenu() {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("show");
+}
