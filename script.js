bebe367e4ecfb3ef0e79504dc8b84acfbab781f9
achino <zakipbg4@gmail.com>
2025-02-27 15:40:33 +0000

    add photos in about me

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toggleMenu() {
     let sidebar = document.getElementById("side");
     sidebar.classList.toggle("open");
+    document.getElementById("side").addEventListener("click", toggleMenu);
 }
-document.getElementById("side").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    document.getElementById("side").style.width = "250px";
+    document.getElementById("nav").style.marginLeft = "250px";
+  }
